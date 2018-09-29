ca7ef23f1fb5df26f55ca8b27c3570256398a11a
Michael Clark <mjc@sifive.com>
2018-09-29 21:16:30 +1200

    Explicitly order HTIF reads and writes

--- machine/htif.c
+++ machine/htif.c
@@ -7,110 +7,78 @@
 #include "syscall.h"
 #include <string.h>
 
-extern uint64_t __htif_base;
-volatile uint64_t tohost __attribute__((section(".htif")));
-volatile uint64_t fromhost __attribute__((section(".htif")));
-volatile int htif_console_buf;
+struct { uint32_t arr[2]; } volatile tohost __attribute__((section(".htif")));
+struct { uint32_t arr[2]; } volatile fromhost __attribute__((section(".htif")));
 static spinlock_t htif_lock = SPINLOCK_INIT;
 uintptr_t htif;
 
-#define TOHOST(base_int)	(uint64_t *)(base_int + TOHOST_OFFSET)
-#define FROMHOST(base_int)	(uint64_t *)(base_int + FROMHOST_OFFSET)
-
-#define TOHOST_OFFSET		((uintptr_t)tohost - (uintptr_t)__htif_base)
-#define FROMHOST_OFFSET		((uintptr_t)fromhost - (uintptr_t)__htif_base)
-
-static void __check_fromhost()
+static inline void htif_send(uint8_t dev, uint8_t cmd, int64_t data)
 {
-  uint64_t fh = fromhost;
-  if (!fh)
-    return;
-  fromhost = 0;
-
-  // this should be from the console
-  assert(FROMHOST_DEV(fh) == 1);
-  switch (FROMHOST_CMD(fh)) {
-    case 0:
-      htif_console_buf = 1 + (uint8_t)FROMHOST_DATA(fh);
-      break;
-    case 1:
-      break;
-    default:
-      assert(0);
-  }
+  /* endian neutral encoding with ordered 32-bit writes */
+  union { uint32_t arr[2]; uint64_t val; } encode = {
+    .val = (uint64_t)dev << 56 | (uint64_t)cmd << 48 | data
+  };
+  tohost.arr[0] = encode.arr[0];
+  tohost.arr[1] = encode.arr[1];
 }
 
-static void __set_tohost(uintptr_t dev, uintptr_t cmd, uintptr_t data)
+static inline void htif_recv(uint8_t *dev, uint8_t *cmd, int64_t *data)
 {
-  while (tohost)
-    __check_fromhost();
-  tohost = TOHOST_CMD(dev, cmd, data);
+  /* endian neutral decoding with ordered 32-bit reads */
+  union { uint32_t arr[2]; uint64_t val; } decode;
+  decode.arr[0] = fromhost.arr[0];
+  decode.arr[1] = fromhost.arr[1];
+  *dev = decode.val >> 56;
+  *cmd = (decode.val >> 48) & 0xff;
+  *data = decode.val << 16 >> 16;
+}
+
+static int64_t htif_get_fromhost(uint8_t dev, uint8_t cmd)
+{
+  /* receive data for specified device and command */
+  uint8_t rdev, rcmd;
+  int64_t data;
+  htif_recv(&rdev, &rcmd, &data);
+  return rdev == dev && rcmd == cmd ? data : -1;
+}
+
+static void htif_set_tohost(uint8_t dev, uint8_t cmd, int64_t data)
+{
+  /* send data with specified device and command */
+  while (tohost.arr[0]) {
+    asm volatile ("" : : "r" (fromhost.arr[0]));
+    asm volatile ("" : : "r" (fromhost.arr[1]));
+  }
+  htif_send(dev, cmd, data);
 }
 
 int htif_console_getchar()
 {
-#if __riscv_xlen == 32
-  // HTIF devices are not supported on RV32
-  return -1;
-#endif
-
+  int ch;
   spinlock_lock(&htif_lock);
-    __check_fromhost();
-    int ch = htif_console_buf;
-    if (ch >= 0) {
-      htif_console_buf = -1;
-      __set_tohost(1, 0, 0);
-    }
+  if ((ch = htif_get_fromhost(1, 0) & 0xff)) {
+    htif_set_tohost(1, 0, 0);
+  }
   spinlock_unlock(&htif_lock);
-
-  return ch - 1;
+  return ch;
 }
 
-static void do_tohost_fromhost(uintptr_t dev, uintptr_t cmd, uintptr_t data)
+void htif_console_putchar(uint8_t ch)
 {
   spinlock_lock(&htif_lock);
-    __set_tohost(dev, cmd, data);
-
-    while (1) {
-      uint64_t fh = fromhost;
-      if (fh) {
-        if (FROMHOST_DEV(fh) == dev && FROMHOST_CMD(fh) == cmd) {
-          fromhost = 0;
-          break;
-        }
-        __check_fromhost();
-      }
-    }
+  htif_set_tohost(1, 1, ch);
   spinlock_unlock(&htif_lock);
 }
 
 void htif_syscall(uintptr_t arg)
 {
-  do_tohost_fromhost(0, 0, arg);
+  htif_set_tohost(0, 0, arg);
 }
 
-void htif_console_putchar(uint8_t ch)
+void htif_poweroff(int status)
 {
-#if __riscv_xlen == 32
-  // HTIF devices are not supported on RV32, so proxy a write system call
-  volatile uint64_t magic_mem[8];
-  magic_mem[0] = SYS_write;
-  magic_mem[1] = 1;
-  magic_mem[2] = (uintptr_t)&ch;
-  magic_mem[3] = 1;
-  do_tohost_fromhost(0, 0, (uintptr_t)magic_mem);
-#else
-  spinlock_lock(&htif_lock);
-    __set_tohost(1, 1, ch);
-  spinlock_unlock(&htif_lock);
-#endif
-}
-
-void htif_poweroff()
-{
-  while (1) {
-    fromhost = 0;
-    tohost = 1;
+  for (;;) {
+    htif_set_tohost(0, 0, 1);
   }
 }
 
